93c9198c5bd27d4917e4dc9db88445a3a8fbdadc
Andrew Zemoytin <andrewzemoytin2005@gmail.com>
2025-07-26 09:12:13 -0700

    Initial commit - UserProfile component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1e1e1e',
+      }}
+    >
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
