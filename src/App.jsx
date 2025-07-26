2811d751393b8917e62b0bcbaee516ceff94e8b5
Azemoytin <andrewzemoytin2005@gmail.com>
2025-07-26 09:21:16 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@ import UserProfile from './UserProfile';
 
 function App() {
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#1e1e1e',
-      }}
-    >
+    <div>
       <UserProfile />
     </div>
   );
